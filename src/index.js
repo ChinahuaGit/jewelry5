34543ca9577254f1dd657fe7468fd891b3a9d375
Kim Johnson <kimeisenmann@gmail.com>
2019-03-15 18:51:13 -0400

    got component jewelryCatalog added and working from the index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Catalog from "../src/component/jewelryCatalog/jewelryCatalog";
 
 import "./myStyles.scss";
 
 const App = () => {
-  console.log("process.env.VERSION", process.env.VERSION);
-  console.log("process.env.PLATFORM", process.env.PLATFORM);
-  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
   return (
     <div className="app">
       <button className="button">Findings</button>
@@ -30,29 +28,7 @@ const App = () => {
             <img alt="header" src="/images/header.jpg" />
             <img alt="header" src="/images/header.jpg" />
             <img alt="header" src="/images/header.jpg" />
-
-            <p className="cartTitleSpacing">
-              <button className="app-cartButton">add to cart</button> $300
-              <button className="app-cartButton">add to cart</button> $300
-              <button className="app-cartButton">add to cart</button> $300
-              <button className="app-cartButton">add to cart</button> $300
-            </p>
-            <img alt="header" src="/images/header.jpg" />
-            <img alt="header" src="/images/header.jpg" />
-            <img alt="header" src="/images/header.jpg" />
-            <img alt="header" src="/images/header.jpg" />
-
-            <p className="cartTitleSpacing">
-              <button className="app-cartButton">add to cart</button> $300
-              <button className="app-cartButton">add to cart</button> $300
-              <button className="app-cartButton">add to cart</button> $300
-              <button className="app-cartButton">add to cart</button> $300
-            </p>
-
-            <img alt="header" src="/images/header.jpg" />
-            <img alt="header" src="/images/header.jpg" />
-            <img alt="header" src="/images/header.jpg" />
-            <img alt="header" src="/images/header.jpg" />
+            <Catalog />
           </span>
         </div>
       </p>
