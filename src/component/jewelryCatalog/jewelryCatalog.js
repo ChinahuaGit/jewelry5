10f6100f3c92e50e136e8114612831f731b3a72c
Kim Johnson <kimeisenmann@gmail.com>
2019-03-15 20:54:41 -0400

    async and await polyfill is working - modify webpack base

--- src/component/jewelryCatalog/jewelryCatalog.js
+++ src/component/jewelryCatalog/jewelryCatalog.js
@@ -1,7 +1,12 @@
 import React from "react";
+//import jewelryCatalogStyles from "./jewelryCatalogStyles";
 
 const Catalog = () => (
   <div>
+    <p className="catalogTitleSpacing">
+      <button className="catalogCartButton">add to cart</button> $300
+      <img alt="header" src="/images/header.jpg" />
+    </p>
     <h1>$300 </h1>
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -1,39 +1,58 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Catalog from "../src/component/jewelryCatalog/jewelryCatalog";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./myStyles.scss";
+import './myStyles.scss';
 
-const App = () => {
-  return (
-    <div className="app">
-      <button className="button">Findings</button>
-      <button className="button">Beads</button>
-      <button className="button">Necklaces</button>
-      <button className="button">Earings</button>
-      <button className="button">About me</button>
-      <h1 className="headerTitle">Garden of Beaden</h1>
-      <p className="headerDiscription">Jewelry ideas, findings, and beads</p>
-      <p>
-        ______________________________________________________________
-        <div>
-          <span class Name="app-body">
-            <p className="cartTitleSpacing">
-              <button className="app-cartButton">add to cart</button> $300
-              <button className="app-cartButton">add to cart</button> $300
-              <button className="app-cartButton">add to cart</button> $300
-              <button className="app-cartButton">add to cart</button> $300
-            </p>
-            <img alt="header" src="/images/header.jpg" />
-            <img alt="header" src="/images/header.jpg" />
-            <img alt="header" src="/images/header.jpg" />
-            <img alt="header" src="/images/header.jpg" />
-            <Catalog />
-          </span>
-        </div>
-      </p>
-    </div>
-  );
-};
+class App extends React.Component {
+  state = {
+    CaptainKirkBio: {},
+  };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+  componentDidMount() {
+    this.onGetKirkBio();
+  }
+
+  onGetKirkBio = async () => {
+    try {
+      const result = await fetch('http://stapi.co/api/v1/rest/character/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: {
+          title: 'James T. Kirk',
+          name: 'James T. Kirk',
+        },
+      });
+      const resultJSON = await result.json();
+      const character = resultJSON.characters[0];
+      this.setState({ CaptainKirkBio: character });
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  render() {
+    const { CaptainKirkBio } = this.state;
+    return (
+      <div className="app">
+        <img alt="header" src="/dist/images/header.jpg" className="app-header" />
+        <p>
+          We are a most promising species, Mr. Spock, as predators go. Did you know that? I
+          frequently have my doubts. I dont. Not any more. And maybe in a thousand years or so, we
+          will be able to prove it.
+        </p>
+        <p>- Captain Kirk</p>
+        <section>
+          {Object.values(CaptainKirkBio).length === 0 ? (
+            <p>Loading User Information</p>
+          ) : (
+            <p style={{ wordBreak: 'break-all' }}>{JSON.stringify(CaptainKirkBio)}</p>
+          )}
+        </section>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
