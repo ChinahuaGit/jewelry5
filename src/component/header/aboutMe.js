b48c7cf6b4376e0e8e3323a4fa457fa1ea96341c
Kim Johnson <kimeisenmann@gmail.com>
2019-03-17 09:40:20 -0400

    router working phayse 1

---
+++ src/component/header/aboutMe.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AboutMe = () => (
+  <div>
+    <h1>This is about me  This is about me This is about me This is about me This is about me This is about me This is about me This is about me This is about me This is about me This is about me This is about me </h1>
+  </div>
+);
+
+export default AboutMe;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import {
+  Router,
+  Route,
+  Link,
+  IndexRoute,
+  hashHistory,
+  browserHistory
+} from "react-router";
 import ReactDOM from "react-dom";
 import Catalog from "../src/component/jewelryCatalog/jewelryCatalog";
+import AboutMe from "../src/component/header/aboutMe";
+//import App from "App";
 
 import "./myStyles.scss";
 
 const App = () => {
   return (
     <div className="app">
+      <Router history={browserHistory}>
+        <Route path="/" component={mHome} />
+        <Route path="/address" component={Address} />
+        <Route path="/aboutMe" component={AboutMe} />
+      </Router>
       <button className="button">Findings</button>
       <button className="button">Beads</button>
       <button className="button">Necklaces</button>
@@ -35,5 +50,6 @@ const App = () => {
     </div>
   );
 };
-
+const mHome = () => <h1>Hello from Home!</h1>;
+const Address = () => <h1>We are located at 555 Jackson St.</h1>;
 ReactDOM.render(<App />, document.getElementById("app"));
